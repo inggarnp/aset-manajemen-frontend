5f816118aa82e0c14619ddb90382e0bb8cf64804
Inggar <macbook@MacBook-Pro.local>
2026-05-18 15:11:14 +0700

    fix vercel routing

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,7 +4,7 @@ const api = axios.create({
   //baseURL: 'http://localhost:8080',
   //baseURL: 'https://2mwnrqhc-8080.asse.devtunnels.ms/',
   //baseURL: "http://10.80.132.123:8080",
-  baseURL: "https://aset-manajemen-backend-production.up.railway.app",
+  baseURL: "https://11830e59-afe7-40c4-917c-6fc2638ff6fb-00-1206pj0e9puai.sisko.replit.dev/",
   headers: {
     "Content-Type": "application/json",
   },
